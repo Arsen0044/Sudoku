76fcabcb4b008e3da543a6d9756cbd4bf739c080
Arsen0044 <arsik04mydzyn@gmail.com>
2023-10-09 07:27:11 -0400

    first commit

--- Buttons.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-def key_pressed(event):
-    key_sym = event.keysym
-    print("Код клавіші:", key_sym)
-
-# Приклад використання
-root = Tk()
-root.bind('<KeyPress>', key_pressed)
-root.mainloop()
